38da5dbd0fa3d6decf93e5b9a2840e4f675b3cfc
opu183059 <opuctg440@gmail.com>
2023-05-02 07:02:10 +0600

    corse installed for local server

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
+
 const allDt = require("./data2.json");
+
 app.get('/allData', (req,res)=>{
     res.send(allDt);
 });
